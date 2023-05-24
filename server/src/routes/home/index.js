878c0f781375f7634a81d2fa7188985e815f199b
ho-sick99 <jhmoon6807@gmail.com>
2023-05-25 00:25:16 +0900

    feat: 서버 테스팅 API 추가

--- server/src/routes/home/index.js
+++ server/src/routes/home/index.js
@@ -2,8 +2,8 @@
 
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-    res.send("hello!");
+router.get("/test", (req, res) => {
+    res.send(req.body);
 })
 
 module.exports = router;
